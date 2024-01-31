6e19723115987215c76bb18a4f180f182277f769
ehsan <ehsan.1383asg63@yahoo.com>
2024-01-31 19:21:41 +0330

    Add model user to project

--- core/models.py
+++ core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .managers import UserManager
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+	email = models.EmailField(max_length=255, unique=True)
+	phone_number = models.CharField(max_length=11, unique=True)
+	full_name = models.CharField(max_length=200)
+	role = models.CharField(max_length=50)
+	image_profile = models.ImageField(upload_to ='uploads/% Y/% m/% d/') 
+	is_active = models.BooleanField(default=True)
+	is_admin = models.BooleanField(default=False)
+
+	objects = UserManager()
+
+	USERNAME_FIELD = 'phone_number'
+	REQUIRED_FIELDS = ['email', 'full_name']
+
+	def __str__(self):
+		return self.email
+
+	@property
+	def is_staff(self):
+		return self.is_admin
--- customer/apps.py
+++ customer/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CartConfig(AppConfig):
+class CustomerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cart'
+    name = 'customer'
