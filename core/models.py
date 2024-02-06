990ec7b29033bab827cc2f5dee6e5615b237e731
ehsan-asg <ehsan.1383asg63@yahoo.com>
2024-02-06 10:50:59 +0330

    Add base model to project

--- core/models.py
+++ core/models.py
@@ -1,25 +1,19 @@
+from typing import Any
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
-from .managers import UserManager
 
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True,null=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted = models.BooleanField(default=False)
 
-class User(AbstractBaseUser):
-	email = models.EmailField(max_length=255, unique=True)
-	phone_number = models.CharField(max_length=11, unique=True)
-	full_name = models.CharField(max_length=200)
-	role = models.CharField(max_length=50)
-	image_profile = models.ImageField(upload_to ='uploads/% Y/% m/% d/') 
-	is_active = models.BooleanField(default=True)
-	is_admin = models.BooleanField(default=False)
+    class Meta:
+        abstract = True
 
-	objects = UserManager()
+    def delete(self):
+        self.deleted = True
+        self.save()
+    
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted=False)
 
-	USERNAME_FIELD = 'phone_number'
-	REQUIRED_FIELDS = ['email', 'full_name']
-
-	def __str__(self):
-		return self.email
-
-	@property
-	def is_staff(self):
-		return self.is_admin
+    
