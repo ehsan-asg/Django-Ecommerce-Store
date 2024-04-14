9d0333f23d5e920fb6e9dd628b3ede8b14281ad3
ehsan-asg <ehsan.1383asg63@yahoo.com>
2024-04-14 18:14:46 +0330

    Change message error form

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,24 +8,25 @@ class AuthenticationForm(auth_forms.AuthenticationForm):
     def confirm_login_allowed(self, user):
         super(AuthenticationForm,self).confirm_login_allowed(user)
 
+
 class SignupForm(forms.Form):
     input_class = ' form-control form-control-lg text-center'
     input_id = "signupSimpleLoginPassword"
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': input_class,"id":input_id}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': input_class,"id":input_id}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': input_class,"id":input_id}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': input_class,"id":input_id}),label='ایمیل')
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': input_class,"id":input_id}),label='کلمه عبور')
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': input_class,"id":input_id}),label='تکرار کلمه عبور')
     
     def clean_email(self):
         email = self.cleaned_data['email']
         user = get_user_model().objects.filter(email=email).exists()
         if user:
-            raise forms.ValidationError('This email already exists')
+            raise forms.ValidationError('این ایمیل از قبل موجود است')
         return email
     
     def clean_password2(self):
         cd = self.cleaned_data
         if cd['password1'] and cd['password2'] and cd['password1'] != cd['password2']:
-            raise forms.ValidationError('Passwords do not match')
+            raise forms.ValidationError('رمزهای ورود مطابقت ندارند')
         return cd['password2']
 
 
